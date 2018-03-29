710450c65a3923cd518bf16f473c90808e4bebea
Catchermax <Catchermax@gmail.com>
2018-03-29 17:01:13 +0300

    HeaderPanelBlock - WebElements: ready,  methods UI: ready
    bisness logic methods for header: in progress

--- src/main/java/io/github/epam/page_objects/blocks/HeaderPanelBlock.java
+++ src/main/java/io/github/epam/page_objects/blocks/HeaderPanelBlock.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 //класс описывает блок хедер
 
@@ -47,12 +48,12 @@ public class HeaderPanelBlock {
     private WebElement differentElementsLink;
 
     @FindBy(xpath = "//header//a[text() = 'Metals & Colors']")
-    private WebElement MetalsAndColorsLink;
+    private WebElement metalsAndColorsLink;
 
-    @FindBy(css = "header div.search>span.icon-search")
+    @FindBy(css = "header div.search>span.icon-search") //иконка лупы
     private WebElement searchIcon;
 
-    @FindBy(css = "header div.search div.search-field input")
+    @FindBy(css = "header div.search div.search-field input") //поле поиска, открывается по иконке лупы
     private WebElement searchField;
 
     @FindBy(css = "header ul.navbar-right a.dropdown-toggle")
@@ -61,13 +62,13 @@ public class HeaderPanelBlock {
     @FindBy(css = "header ul.navbar-right div.dropdown-menu-login div.logout button") //кнопка логаута доступна только для авторизованных пользователю
     private WebElement logoutButton;
 
-    @FindBy(css = "header ul.navbar-right div.dropdown-menu-login form#login-form input#Name") //поле логина, доступно только неавторизованному пользователю
+    @FindBy(css = "header ul.navbar-right div.dropdown-menu-login form#login-form input#Name") //поле логина, доступно только НЕавторизованному пользователю
     private WebElement loginField;
 
-    @FindBy(css = "header ul.navbar-right div.dropdown-menu-login form#login-form input#Password") //поле пароля, доступно только неавторизованному пользователю
+    @FindBy(css = "header ul.navbar-right div.dropdown-menu-login form#login-form input#Password") //поле пароля, доступно только НЕавторизованному пользователю
     private WebElement passwordField;
 
-    @FindBy(css = "header ul.navbar-right div.dropdown-menu-login form#login-form button.btn-login") //кнока Enter, доступно только неавторизованному пользователю
+    @FindBy(css = "header ul.navbar-right div.dropdown-menu-login form#login-form button.btn-login") //кнока Enter, доступно только НЕавторизованному пользователю
     private WebElement enterButton;
 
 
@@ -76,4 +77,76 @@ public class HeaderPanelBlock {
     public void homeLinkClick(){
         homeLink.click();
     }
+
+    public void contactFormLinkClick(){
+        contactFormLink.click();
+    }
+
+    public void supportLinkClick(){
+        serviceDropDown.click();
+        supportLink.click();
+    }
+
+    public void datesLinkClick(){
+        serviceDropDown.click();
+        supportLink.click();
+    }
+
+    public void complexTableLinkClick(){
+        serviceDropDown.click();
+        complexTableLink.click();
+    }
+
+    public void simpleTableLinkClick(){
+        serviceDropDown.click();
+        simpleTableLink.click();
+    }
+
+    public void userTableLinkClick(){
+        serviceDropDown.click();
+        userTableLink.click();
+    }
+
+    public void tableWithPagesLinkClick(){
+        serviceDropDown.click();
+        tableWithPagesLink.click();
+    }
+
+    public void differentElementsLinkClick(){
+        serviceDropDown.click();
+        differentElementsLink.click();
+    }
+
+    public void MetalsAndColorsLinkClick(){
+        metalsAndColorsLink.click();
+    }
+
+    public void searchIconClick(){
+        searchIcon.click();
+    }
+
+    public void searchFieldSendKeys(String request){ //ввести в поле поиска запрос, доступно только после нажатия иконки лупы
+        searchField.sendKeys(request);
+    }
+
+    public void loginDropdownClick(){
+        loginDropdown.click();
+    }
+
+    public void loginFieldSendKeys(String login){ //заполнить поле логин, только для НЕавторизованного пользователя
+        loginField.sendKeys(login);
+    }
+
+    public void passwordFieldSendKeys(String password){ //заполнить поле пароль, только для НЕавторизованного пользователя
+        passwordField.sendKeys(password);
+    }
+
+    public void enterButtonClick(){ //нажать кнопку Enter, только для НЕавторизованного пользователя
+        enterButton.click();
+    }
+
+    public void logoutButtonClick(){ //нажать кнопку logout, только для авторизованного пользователя
+        logoutButton.click();
+    }
+
 }
