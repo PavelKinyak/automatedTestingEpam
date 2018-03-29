738facdc637eb144909e45d299203bdce690b5e0
Tinary <tinaryderre@list.ru>
2018-03-29 13:23:30 +0300

    test

--- src/main/java/io/github/epam/page_objects/pages/ContactFormPage.java
+++ src/main/java/io/github/epam/page_objects/pages/ContactFormPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 /*класс описывает страницу https://epam.github.io/JDI/contacts.html , только то что находится на этой странице, не включает в себя хедер, футор, блоки слева и справа*/
+//sfddsfsfd
 
 public class ContactFormPage {
     WebDriver driver;
