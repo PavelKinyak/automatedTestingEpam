e6bc1b1d306bfd711333a789cfd8e15ff496f6fc
Tester <v.avdeev@premiumitsolution.com>
2018-03-30 11:46:29 +0300

    FootterPanelBlock in progress Change BaseClass

--- src/main/java/base/BaseDriverClass.java
+++ src/main/java/base/BaseDriverClass.java
@@ -1,6 +1,7 @@
 package base;
 
 import io.github.epam.page_objects.bisnessLogic.BisnessLogicHeaderPanelBlock;
+import io.github.epam.page_objects.blocks.FootterPanelBlock;
 import io.github.epam.page_objects.blocks.HeaderPanelBlock;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,10 +12,13 @@ public class BaseDriverClass {
     public static WebDriver driver;
     //добавляем классы с бизнес-логикой
     protected static BisnessLogicHeaderPanelBlock bisnessLogicHeaderPanelBlock;
+    protected static FootterPanelBlock footterPanelBlock;
 
 
     protected static void setupDriver(){
         driver = new ChromeDriver();
+        //инициализация страниц
+        footterPanelBlock = new FootterPanelBlock(driver);
         //инициализируем классы с бизнес-логикой
         bisnessLogicHeaderPanelBlock = new BisnessLogicHeaderPanelBlock(driver);
         //
