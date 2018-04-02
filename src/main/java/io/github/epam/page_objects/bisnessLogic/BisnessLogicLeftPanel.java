f65c96dcdee8a2c76b3aeba15434c6061b961580
PavelKinyak <kpa15071990>
2018-04-02 17:02:12 +0300

    LeftPanel - ready

---
+++ src/main/java/io/github/epam/page_objects/bisnessLogic/BisnessLogicLeftPanel.java
@@ -0,0 +1,58 @@
+package io.github.epam.page_objects.bisnessLogic;
+
+import io.github.epam.page_objects.blocks.LeftPanel;
+import org.openqa.selenium.WebDriver;
+
+public class BisnessLogicLeftPanel extends LeftPanel{
+
+    public BisnessLogicLeftPanel(WebDriver driver) {
+        super(driver);
+}
+    public void openPageHome(){ //открыть странцу Home
+        homeSpan();
+}
+    public void openPageContactForm(){
+        contactFormSpan();
+}
+    public void openPageService(){ //открыть странцу Service
+        serviceSpan();
+}
+    public void openPageSupport(){ //открыть странцу Support
+        supportSpan();
+}
+    public void openPageDates(){ //открыть странцу Dates
+        datesSpan();
+}
+    public void openPageComplexTable(){ //открыть странцу ComplexTable
+        complexTableSpan();
+}
+    public void openPageSimpleTable(){ //открыть странцу SimpleTable
+        simpleTableSpan();
+}
+    public void openUserTable(){ //открыть странцу UserTable
+        userTableSpan();
+}
+    public void openPageTableWithPages(){ //открыть странцу TableWithPages
+        tableWithPagesSpan();
+}
+    public void openPageDifferentElements(){ //открыть странцу DifferentElements
+        differentElementsSpan();
+}
+    public void openMetalsColors(){ //открыть странцу MetalsColors
+        metalsColorsSpan();
+}
+
+
+        }
+
+
+        }
+
+
+        }
+
+
+        }
+
+
+        }
